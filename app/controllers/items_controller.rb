0014c5e0644eb2287228caeb500279755a9f6be8
Ben Purinton <ben.purinton@gmail.com>
2025-08-04 14:07:01 -0700

    rubocop -A

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,7 +15,7 @@ class ItemsController < ApplicationController
     new_item.link_url = params.fetch("query_link_url")
     new_item.link_description = params.fetch("query_link_description")
     new_item.thumbnail_url = params.fetch("query_thumbnail_url")
-  
+
     new_item.save
 
     redirect_to("/")
